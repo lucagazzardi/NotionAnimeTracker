713f1d379b413ad4401c55aba472e28aa94e48d0
Luca Gazzardi <gazzardi.luca@gmail.com>
2023-07-27 13:52:23 +0200

    History completed

--- UI-AnimeToNotion/src/components/history/history.component.ts
+++ UI-AnimeToNotion/src/components/history/history.component.ts
@@ -1,16 +1,26 @@
+import { transition, trigger, useAnimation } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { opacityOnEnter } from '../../assets/animations/animations';
 import { IHistory } from '../../model/IHistory';
 import { InternalService } from '../../services/internal/internal.service';
 
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
-  styleUrls: ['./history.component.scss']
+  styleUrls: ['./history.component.scss'],
+  animations: [
+    trigger('opacityOnEnter', [
+      transition(':enter', [
+        useAnimation(opacityOnEnter)
+      ])
+    ]),
+  ]
 })
 export class HistoryComponent implements OnInit {
 
-  historyYears$!: Observable<IHistory[]>
+  historyYears$!: Observable<IHistory[]>;
+  skeletonYears = Array(6).fill(0);
 
   constructor(private internalService: InternalService) { }
 
